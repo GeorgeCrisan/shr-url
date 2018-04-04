e575a285b14b09897f69394756234c583a953a34
Glitch (shr-url) <none>
2018-04-04 10:10:30 +0000

    good

--- public/client.js
+++ public/client.js
@@ -1,5 +1,17 @@
 function sendUri(){
    let url = document.getElementById('toTransform');
+   let value = url.value;
+   console.log(value);
+  $.post("/new/" + encodeURI(value),(success)=>{
+  
+          console.log(success);
+  })
+    .done(()=> { value = null; } )
+    
+    .fail((err)=> console.log(err))
+   
+    .always(()=>{console.log('reached always')});
+  
   
   
 }
